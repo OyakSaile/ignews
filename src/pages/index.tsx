44ef3a6323dcdb2f9b6371c47581a3ee3dececd2
Kayo Elias <contatodevkayo@gmail.com>
2022-04-14 18:57:44 -0300

    Página Home

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>íncio | ig.news</title>
+      </Head>
+
+      <h1>Hello, World!</h1>
+    </>
+  );
+}
